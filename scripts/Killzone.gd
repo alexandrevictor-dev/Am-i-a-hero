0c18f7d10f0969dee39b689fe56bdea02a282e83
Alexandre Victor <169073432+halliontk@users.noreply.github.com>
2024-06-06 23:07:28 -0300

    killzone

---
+++ scripts/Killzone.gd
@@ -0,0 +1,14 @@
+extends Area2D
+@onready var timer = $Timer
+
+
+func _on_body_entered(body):
+	print('morriiiiiiiiiiiiii')
+	timer.start()
+
+
+
+
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
+	
