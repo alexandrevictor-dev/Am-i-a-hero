f0c96800a462ebd87b5ab88407b3c5d7357aa933
Alexandre Victor <169073432+halliontk@users.noreply.github.com>
2024-06-14 18:29:09 -0300

    1

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 @onready var animated_sprite = $AnimatedSprite2D
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 110.0
+const JUMP_VELOCITY = -230.0
+var MAX_JUMP = 2
+var jump_count = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,10 +14,13 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		jump_count = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (jump_count < MAX_JUMP):
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 
 	# Get the input direction 
 	var direction = Input.get_axis("move_left", "move_right")
